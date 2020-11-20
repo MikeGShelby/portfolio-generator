7e645efc2962ecf72088789042442deef84c738b
Mike Shelby <mikegshelby@gmail.com>
2020-11-20 16:15:46 -0600

    link inquirer module to app.js

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+// *** DEPENDENCIES ***
+// npm packages
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+// personal modules
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// core library modules
+// const fs = require('fs');
+
+// const pageHTML = generatePage(name, github);
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
